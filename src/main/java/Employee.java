0a3b55bac90c28b5948ba534fd83f6328ef13ab3
CiaranSlattery1212 <g00373551@gmit.ie>
2022-03-08 10:43:46 +0000

    Test Cases for Employee Class

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,7 +1,64 @@
 
-
 public class Employee {
-    
 
+//    public Object employee(String title, String name, int ppsID, int phone, String EmploymentType, int age){
+//
+//        return title;
+//    }
+
+    public String employeeTitle(String title){
+        if(title != "Mrs"){
+            return ("Invalid Title");
+        }
+        return title;
+    }
+
+    public String employeeName(String name){
+        if(name != "Daisy"){
+            return ("Invalid Name");
+        }
+        return name;
+    }
+
+    public Object employeeID(int id){
+
+        if(id != 123456){
+            return ("Invalid User ID");
+        }
+
+        return id;
+    }
+
+    public Object employeePhone(int phoneNum){
+
+        if(phoneNum != 1234567){
+            return ("Invalid Phone Number");
+        }
+
+        return phoneNum;
+    }
+
+    public String employementType(String type){
+
+        if(type == "Full-time"){
+            return type;
+        }
+        else if(type == "Part-time"){
+            return type;
+        }
+        else {
+            return ("Invalid Employment Type");
+        }
+
+    }
+
+    public Object employeeAge(int age){
+
+        if(age < 18){
+            return ("Invalid Age, Age must be over 18");
+        }
+
+        return age;
+    }
 
 }
